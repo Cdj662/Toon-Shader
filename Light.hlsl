da5c60a4bdf5a44120cc34f6e56cc57d9aacab72
Courtney <72889576+Cdj662@users.noreply.github.com>
2022-03-17 17:26:27 -0700

    Add files via upload

---
+++ Light.hlsl
@@ -0,0 +1,13 @@
+﻿void LightInformation_float(out float3 Direction, out float3 Color, out float Attenuation)
+{
+#ifdef
+	Direction = float3(-0.2, 0.2, -0.2);
+	Color = float3(1, 1, 1);
+	Attenuation = 0.6;
+#else
+	Light light = Light();
+	Direction = light.direction;
+	Attenuation = light.distanceAttenuation;
+	Color = light.color;
+#endif
+}
